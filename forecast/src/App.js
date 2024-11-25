1657dec19175f6c227d8abbee3b0b5bc72da498b
renzcpgcn <capagcuanrenz13@gmail.com>
2024-11-25 09:38:46 +0800

    Up

--- forecast/src/App.js
+++ forecast/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import './styles/App.css';
+// import './App.css';
 import FileUpload from './Components/FileUpload';
 import ProductSelector from './Components/ProductSelector';
 import Preprocessing from './Components/Preprocessing';
